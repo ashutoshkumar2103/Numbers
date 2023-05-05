deab62db2a35607f76043a50698a4056702b9cb7
Ashutosh Kumar <ashu80934@gmail.com>
2023-05-05 17:40:34 +0530

    Update and rename SplNum.java to SpecialNum.java

--- Number Practice/SpecialNum.java
+++ Number Practice/SpecialNum.java
@@ -1,6 +1,6 @@
 // A special two-digit number is such that when the sum of its digits is added to the product of its digits, the result is equal to the original two-digit number.
 import java.util.Scanner;	//59
-public class SplNum {
+public class SpecialNum {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Number: ");
